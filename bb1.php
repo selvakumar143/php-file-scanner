7911792920e987a653fd080aeb0c0a6f5be2f2ca
selvakumar <sp.selvakumar2012@gmail.com>
2017-08-23 22:58:28 +0530

    read the databse throw while

---
+++ bb1.php
@@ -0,0 +1,22 @@
+<?php
+	$servername = "localhost";
+	$username = "sasivarn_gbro";
+	$password = "znp=X6qFPmR,";
+	$dbname = "sasivarn_gbro";
+	$con=mysqli_connect($servername,$username,$password,$dbname);
+	if (!$con)
+	{
+		die('Could not connect:'.mysql_error());
+	}
+	$query="select * from file_size1 limit 10";
+	$res=mysqli_query($con,$query);
+	while($row=$res->fetch_assoc())
+	{
+		print($row['id']);
+		print($row['file_name']);
+		print('<br>');
+	}
+	mysqli_close($con);
+	
+		
+?>
